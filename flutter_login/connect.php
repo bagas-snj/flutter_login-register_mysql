99da4e043a12abf0ccb8c8a25d1fa800f74de442
bagasrth <bagasrth@gmail.com>
2020-11-30 19:35:47 +0700

    Aplikasi login dan register menggunakan flutter

---
+++ flutter_login/connect.php
@@ -0,0 +1,3 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', '', 'flutter_login') or die('tidak terkoneksi');
+?>
---
+++ lib/api.dart
@@ -0,0 +1,4 @@
+class Api {
+  static String url = "http://127.0.0.1:8080/flutter_login/check.php";
+  static String regis = "http://127.0.0.1:8080/flutter_login/register.php";
+}
